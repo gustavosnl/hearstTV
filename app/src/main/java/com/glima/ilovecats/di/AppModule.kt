d8bb93ecbcbe3513b9c9d047ee652bddae15304d
Gustavo Lima <gustavosnl@gmail.com>
2023-02-16 23:06:42 -0300

    Setup Retrofit

--- app/src/main/java/com/glima/ilovecats/di/AppModule.kt
+++ app/src/main/java/com/glima/ilovecats/di/AppModule.kt
@@ -1,9 +1,47 @@
 package com.glima.ilovecats.di
 
+import com.glima.ilovecats.BuildConfig
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 object AppModule {
     val appModule = module {
+        single<Retrofit> {
+            Retrofit.Builder()
+                .baseUrl(BuildConfig.SERVER_URL)
+                .addConverterFactory(MoshiConverterFactory.create(get()))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .client(get())
+                .build()
+        }
 
+        single {
+            Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+        }
+
+        single {
+            val interceptor = HttpLoggingInterceptor().apply {
+                level = if (BuildConfig.DEBUG) Level.BODY else Level.NONE
+            }
+            OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .addInterceptor { chain: Interceptor.Chain ->
+                    val request = chain.request().newBuilder()
+                        .header("x-api-key", BuildConfig.API_KEY)
+                        .build()
+                    chain.proceed(request)
+                }
+                .build()
+        }
     }
 }
